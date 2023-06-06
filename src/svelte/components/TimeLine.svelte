b61d1ea2ea97e42ea50def593b426cdbe6d02dac
YukiGasai <r.lindede@googlemail.com>
2023-06-06 17:29:27 +0200

    #129 fixed hour line z index

--- src/svelte/components/TimeLine.svelte
+++ src/svelte/components/TimeLine.svelte
@@ -1,199 +1,217 @@
-<script lang="ts" >
-import timerStore from '../extra/timerStore'
-import type { GoogleEvent } from "../../helper/types";
-import { quintOut } from 'svelte/easing';
-import { crossfade } from 'svelte/transition';
-import TreeMap from 'ts-treemap'
-import { dateToPercent, getStartHeightOfHour, getEndHeightOfHour } from "../../helper/Helper";
-import {getEventStartPosition, getEventHeight} from "../../helper/Helper";
-import {getColorFromEvent} from '../../googleApi/GoogleColors'
-import GoogleCalendarPlugin from "../../GoogleCalendarPlugin";
+<script lang="ts">
+	import timerStore from '../extra/timerStore';
+	import type { GoogleEvent } from '../../helper/types';
+	import { quintOut } from 'svelte/easing';
+	import { crossfade } from 'svelte/transition';
+	import TreeMap from 'ts-treemap';
+	import {
+		dateToPercent,
+		getStartHeightOfHour,
+		getEndHeightOfHour,
+	} from '../../helper/Helper';
+	import { getEventStartPosition, getEventHeight } from '../../helper/Helper';
+	import { getColorFromEvent } from '../../googleApi/GoogleColors';
+	import GoogleCalendarPlugin from '../../GoogleCalendarPlugin';
 
-interface Location {
-    event:GoogleEvent;
-    x:number;
-    y:number;
-    width:number;
-    height:number; 
-    fullDay:boolean;
-}
+	interface Location {
+		event: GoogleEvent;
+		x: number;
+		y: number;
+		width: number;
+		height: number;
+		fullDay: boolean;
+	}
 
-export let height = 700;
-export let width = 300;
-export let events: GoogleEvent[];
-export let date;
-export let hourRange;
-export let goToEvent;
+	export let height = 700;
+	export let width = 300;
+	export let events: GoogleEvent[];
+	export let day;
+	export let hourRange;
+	export let goToEvent;
 
-const getRedTimeLinePosition = (time) => {
-    const dayPercentage = dateToPercent(time);
-    return Math.floor(height * dayPercentage);
-}
+	const getRedTimeLinePosition = (time) => {
+		const dayPercentage = dateToPercent(time);
+		return Math.floor(height * dayPercentage);
+	};
 
-const plugin = GoogleCalendarPlugin.getInstance();
-let hourFormat = plugin.settings.timelineHourFormat;
-const currentTime = timerStore();
+	const plugin = GoogleCalendarPlugin.getInstance();
+	let hourFormat = plugin.settings.timelineHourFormat;
+	const currentTime = timerStore();
 
-const getLocationArray = (events) => {
-    let eventLocations:Location[] = [];
-    const startMap = new TreeMap<string, GoogleEvent[]>();
-    events.forEach((event) => {
-        const start = event.start.date || event.start.dateTime;
-        if(startMap.has(start)){
-            startMap.get(start).push(event)
-        }else{
-            startMap.set(start, [event])
-        }
-    });
+	const getLocationArray = (events) => {
+		let eventLocations: Location[] = [];
+		const startMap = new TreeMap<string, GoogleEvent[]>();
+		events.forEach((event) => {
+			const start = event.start.date || event.start.dateTime;
+			if (startMap.has(start)) {
+				startMap.get(start).push(event);
+			} else {
+				startMap.set(start, [event]);
+			}
+		});
 
-    let indentAmount = 0;
-    let latestEndDate = null; 
+		let indentAmount = 0;
+		let latestEndDate = null;
 
-    for (let events of startMap.values()) {
+		for (let events of startMap.values()) {
+			if (events[0].start.dateTime) {
+				const startDate = window.moment(events[0].start.dateTime);
 
-        if(events[0].start.dateTime){
-            const startDate = window.moment(events[0].start.dateTime)
-            
-            if(latestEndDate && startDate.isBefore(latestEndDate, "minutes")){
-                indentAmount++;
-            }else{
-                indentAmount = 0;
-            }
+				if (latestEndDate && startDate.isBefore(latestEndDate, 'minutes')) {
+					indentAmount++;
+				} else {
+					indentAmount = 0;
+				}
 
-            latestEndDate = window.moment(events[0].end.dateTime)
-        }
-        events.forEach((event, i) => {
-                        
-            const indent = indentAmount * 5;
+				latestEndDate = window.moment(events[0].end.dateTime);
+			}
+			events.forEach((event, i) => {
+				const indent = indentAmount * 5;
 
-            const elementWidth = (100-indent) / events.length;
-            eventLocations = [...eventLocations, {
-                event: event,
-                x: indent + elementWidth*i,
-                y: getEventStartPosition(event, height),
-                width: elementWidth,
-                height: getEventHeight(event, height),
-                fullDay: event.start.date != undefined
-            }]     
-        })
-    }
-    return eventLocations;
-}
+				const elementWidth = (100 - indent) / events.length;
+				eventLocations = [
+					...eventLocations,
+					{
+						event: event,
+						x: indent + elementWidth * i,
+						y: getEventStartPosition(event, height),
+						width: elementWidth,
+						height: getEventHeight(event, height),
+						fullDay: event.start.date != undefined,
+					},
+				];
+			});
+		}
+		return eventLocations;
+	};
 
-const [send, receive] = crossfade({
-    duration: d => Math.sqrt(d * 200),
+	const [send, receive] = crossfade({
+		duration: (d) => Math.sqrt(d * 200),
 
-    fallback(node, params) {
-        const style = getComputedStyle(node);
-        const transform = style.transform === 'none' ? '' : style.transform;
+		fallback(node, params) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
 
-        return {
-            duration: 600,
-            easing: quintOut,
-            css: t => `
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: (t) => `
                 transform: ${transform} scale(${t});
                 opacity: ${t}
-            `
-        };
-    }
-});
-
+            `,
+			};
+		},
+	});
 </script>
 
-<div 
-    style:height="{height}px"
-    style:width="{width}px"
-    style:max-width="100%"
-    style:margin=" -{getStartHeightOfHour(height, hourRange[0])}px 0px -{getEndHeightOfHour(height, hourRange[1])}px 0px"
-    class="gcal-timeline"
-    >
-    <div class="gcal-timeline-container">
-        <div class="gcal-hour-line-container">
-            {#each {length: 24} as _, i }
-                <div class={hourFormat > 3 ? "gcal-hour-line gcal-hour-line-large" : "gcal-hour-line"} style:height="{height/24}px" />
-            {/each}
-        </div>  
-    </div>
+<div
+	style:height="{height}px"
+	style:width="{width}px"
+	style:max-width="100%"
+	style:margin=" -{getStartHeightOfHour(height, hourRange[0])}px 0px -{getEndHeightOfHour(
+		height,
+		hourRange[1]
+	)}px 0px"
+	class="gcal-timeline"
+>
+	<div class="gcal-timeline-container">
+		<div class="gcal-hour-line-container">
+			{#each { length: 24 } as _, i}
+				<div
+					class={hourFormat > 3
+						? 'gcal-hour-line gcal-hour-line-large'
+						: 'gcal-hour-line'}
+					style:height="{height / 24}px"
+				/>
+			{/each}
+		</div>
+	</div>
 
-{#if window.moment().isSame(date, 'day')}
-    <div class="gcal-time-display" style:top="{getRedTimeLinePosition($currentTime)}px"/>
-{/if}
+	{#if window.moment().isSame(day, 'day')}
+		<div
+			class="gcal-time-display"
+			style:top="{getRedTimeLinePosition($currentTime)}px"
+		/>
+	{/if}
 
-    {#each getLocationArray(events) as location, i (i)}
-        <div 
-            in:receive="{{key: i}}"
-            on:click={(e) => goToEvent(location.event,e)} 
-            on:keypress={(e) => goToEvent(location.event,e)}
-            class="
+	{#each getLocationArray(events) as location, i (i)}
+		<div
+			in:receive={{ key: i }}
+			on:click={(e) => goToEvent(location.event, e)}
+			on:keypress={(e) => goToEvent(location.event, e)}
+			class="
                 googleCalendarEvent
-                googleCalendarEvent_Calendar_Color_{location.event.parent.colorId}
+                googleCalendarEvent_Calendar_Color_{location.event.parent
+				.colorId}
                 googleCalendarEvent_Event_Color_{location.event.parent.colorId}
                 googleCalendarEvent_Id_{location.event.parent.id}
                 "
-            id="{location.event.id}"
-            style:top="{location.y}px"
-            style:left="{location.x}%"
-            style:width="{location.width}%"
-            style:height="{location.height}px"
-            style:background={getColorFromEvent(location.event)}
-        >
-        <span class="
+			id={location.event.id}
+			style:top="{location.y}px"
+			style:left="{location.x}%"
+			style:width="{location.width}%"
+			style:height="{location.height}px"
+			style:background={getColorFromEvent(location.event)}
+		>
+			<span
+				class="
             googleCalendarName
             googleCalendarName_Calendar_Color_{location.event.parent.colorId}
             googleCalendarName_Event_Color_{location.event.parent.colorId}
             googleCalendarName_Id_{location.event.parent.id}
-            ">{location.event.summary}</span>
-    </div>
-    {/each}
-
+            ">{location.event.summary}</span
+			>
+		</div>
+	{/each}
 </div>
 
 <style>
-    .googleCalendarEvent{
-        display: flex;
-        padding: 0 10px 10px;
-        position: absolute;
-        cursor: pointer;
-        width: 150px;
-        border-radius: 5px;
-        color: black;
-        font-size: small;
-        box-shadow: 3px 2px 8px 4px rgba(0,0,0,0.36);
-        overflow: hidden;
-    }
+	.googleCalendarEvent {
+		display: flex;
+		padding: 0 10px 10px;
+		position: absolute;
+		cursor: pointer;
+		width: 150px;
+		border-radius: 5px;
+		color: black;
+		font-size: small;
+		box-shadow: 3px 2px 8px 4px rgba(0, 0, 0, 0.36);
+		overflow: hidden;
+	}
 
-    .gcal-timeline-container {
-        display: flex;
-        gap: 5px;
-    }
+	.gcal-timeline-container {
+		display: flex;
+		gap: 5px;
+	}
 
-    .gcal-hour-line::after{
-        content: "";
-        position: absolute;
-        width: 100%;
-        border-bottom: 1px solid grey;
-    }
+	.gcal-hour-line::after {
+		content: '';
+		position: absolute;
+		width: 100%;
+		border-bottom: 1px solid grey;
+	}
 
-    .gcal-timeline, .hourText{
-        overflow: hidden;
-    }
-            
-    .gcal-time-display{
-        position: absolute;
-        width: 95%;
-        height: 3px;
-        background:red;
-        overflow: visible;
-    }
+	.gcal-timeline,
+	.hourText {
+		overflow: hidden;
+	}
 
-    .gcal-timeline{
-        position:relative;
-        display: flex;
-        flex-direction: row;
-        padding-top: 5px;
-        flex-shrink: 1000;
-        min-height: 0;
-        overflow: hidden; 
-    }
+	.gcal-time-display {
+		position: absolute;
+		width: 95%;
+		height: 3px;
+		background: red;
+		overflow: visible;
+		z-index: 1;
+	}
 
+	.gcal-timeline {
+		position: relative;
+		display: flex;
+		flex-direction: row;
+		padding-top: 5px;
+		flex-shrink: 1000;
+		min-height: 0;
+		overflow: hidden;
+	}
 </style>
--- src/svelte/views/TimeLineView.svelte
+++ src/svelte/views/TimeLineView.svelte
@@ -1,184 +1,196 @@
-<script lang="ts" >
-    // Week view replaces the day view when the timespan is set to 1
+<script lang="ts">
+	// Week view replaces the day view when the timespan is set to 1
 
-    import TimeLine from "../components/TimeLine.svelte";
-    import TimeLineHourText from "../components/TimeLineHourText.svelte";
-	import type { CodeBlockOptions, GoogleEvent } from "../../helper/types";
-	import ViewSettings from "../components/ViewSettings.svelte";
-	import DayNavigation from "../components/DayNavigation.svelte";
-	import { onDestroy } from "svelte";
-	import { googleClearCachedEvents, listEvents } from "../../googleApi/GoogleListEvents";
-	import { EventDetailsModal } from "../../modal/EventDetailsModal";
-	import AllDayContainer from "../components/AllDayContainer.svelte";
-    
+	import TimeLine from '../components/TimeLine.svelte';
+	import TimeLineHourText from '../components/TimeLineHourText.svelte';
+	import type { CodeBlockOptions, GoogleEvent } from '../../helper/types';
+	import ViewSettings from '../components/ViewSettings.svelte';
+	import DayNavigation from '../components/DayNavigation.svelte';
+	import { onDestroy } from 'svelte';
+	import {
+		googleClearCachedEvents,
+		listEvents,
+	} from '../../googleApi/GoogleListEvents';
+	import { EventDetailsModal } from '../../modal/EventDetailsModal';
+	import AllDayContainer from '../components/AllDayContainer.svelte';
 
-    export let codeBlockOptions: CodeBlockOptions;
-    export let isObsidianView = false;
-    export let showSettings = false;
+	export let codeBlockOptions: CodeBlockOptions;
+	export let isObsidianView = false;
+	export let showSettings = false;
 
-    let startDate:moment.Moment = codeBlockOptions.date ? window.moment(codeBlockOptions.date) : window.moment();
-    let dateOffset = 0;
-    let date;
-    let loading = false;
-    let events:GoogleEvent[] = [];
-    let interval;
+	let startDate: moment.Moment = codeBlockOptions.date
+		? window.moment(codeBlockOptions.date)
+		: window.moment();
+	let dateOffset = 0;
+	let date;
+	let loading = false;
+	let events: GoogleEvent[] = [];
+	let interval;
 
+	const getEvents = async (date: moment.Moment) => {
+		if (!date?.isValid()) {
+			loading = false;
+			return;
+		}
 
+		const newEvents = await listEvents({
+			startDate: date,
+			endDate: date.clone().add(codeBlockOptions.timespan, 'days'),
+			include: codeBlockOptions.include,
+			exclude: codeBlockOptions.exclude,
+		});
 
-    const getEvents = async(date:moment.Moment) => {
-        
-        if(!date?.isValid()){
-            loading = false;
-            return;
-        }
-    
-        const newEvents = await listEvents({
-            startDate:date,
-            endDate: date.clone().add(codeBlockOptions.timespan, "days"),
-            include: codeBlockOptions.include,
-            exclude: codeBlockOptions.exclude
-        }); 
-    
-        if(JSON.stringify(newEvents) != JSON.stringify(events)){
-            events = newEvents;
-        }
-    }
+		if (JSON.stringify(newEvents) != JSON.stringify(events)) {
+			events = newEvents;
+		}
+	};
 
-    const refreshData = async (date:moment.Moment) => {
-        if(loading) return;
-        loading = true;
-        await getEvents(date)
-        loading = false;
-    } 
+	const refreshData = async (date: moment.Moment) => {
+		if (loading) return;
+		loading = true;
+		await getEvents(date);
+		loading = false;
+	};
 
-    const goToEvent = (event:GoogleEvent, e:any) => {
-        if(e.shiftKey){
-            window.open(event.htmlLink);
-        }else{
-            new EventDetailsModal(event, () => {
-                googleClearCachedEvents();
-                    refreshData(date);
-            }).open();
-        }
-    }
+	const goToEvent = (event: GoogleEvent, e: any) => {
+		if (e.shiftKey) {
+			window.open(event.htmlLink);
+		} else {
+			new EventDetailsModal(event, () => {
+				googleClearCachedEvents();
+				refreshData(date);
+			}).open();
+		}
+	};
 
-    $: {
-        startDate = codeBlockOptions.date 
-        ? window.moment(codeBlockOptions.date).add(codeBlockOptions.offset, "days") 
-        : window.moment().add(codeBlockOptions.offset, "days");
-        date = codeBlockOptions.navigation ? startDate.clone().local().add(dateOffset, "days") : startDate;
+	$: {
+		startDate = codeBlockOptions.date
+			? window
+					.moment(codeBlockOptions.date)
+					.add(codeBlockOptions.offset, 'days')
+			: window.moment().add(codeBlockOptions.offset, 'days');
+		date = codeBlockOptions.navigation
+			? startDate.clone().local().add(dateOffset, 'days')
+			: startDate;
 
-        if(interval) clearInterval(interval);
-        interval = setInterval(() =>refreshData(date), 5000);
-        refreshData(date);
-    }
-    
-    const getDatesToDisplay = (date) => {
-        let datesToDisplay = [];
+		if (interval) clearInterval(interval);
+		interval = setInterval(() => refreshData(date), 5000);
+		refreshData(date);
+	}
 
-        for (let i = 0; i < codeBlockOptions.timespan; i++) {
-            datesToDisplay = [...datesToDisplay, date.clone().add(i, "days")];
-        }
+	const getDatesToDisplay = (date) => {
+		let datesToDisplay = [];
 
-        return datesToDisplay;
-    }
+		for (let i = 0; i < codeBlockOptions.timespan; i++) {
+			datesToDisplay = [...datesToDisplay, date.clone().add(i, 'days')];
+		}
 
-    onDestroy(() => {
-        clearInterval(interval);
-    })
+		return datesToDisplay;
+	};
 
-    </script>
-    {#if isObsidianView}
-        <ViewSettings bind:codeBlockOptions bind:showSettings/>
-    {/if}
-    <div style="padding-left: 10px;">
-        {#if codeBlockOptions.navigation && date}
-            <DayNavigation bind:dateOffset bind:date bind:startDate />
-        {/if}
+	onDestroy(() => {
+		clearInterval(interval);
+	});
+</script>
 
-        <div 
-            class="gcal-week-container"
-            style:grid-template-columns="auto repeat({codeBlockOptions.timespan}, minmax(0, 1fr))"
-        >
-            {#if codeBlockOptions.timespan > 1}
-                <div class="gcal-stop-overflow" />
-                {#each getDatesToDisplay(date) as day, i}
-                    <div class="gcal-day-container">
-                        <span class="gcal-dayofweek">{day.format('ddd')}</span>
-                        <span class="gcal-day">{day.format('D')}</span>
-                    </div>
-                {/each}
-            {:else}
-            <!-- Required if no header elements are displayed -->
-                <div class="gcal-stop-overflow" />
-                <div class="gcal-stop-overflow" />
-            {/if}
+{#if isObsidianView}
+	<ViewSettings bind:codeBlockOptions bind:showSettings />
+{/if}
+<div style="padding-left: 10px;">
+	{#if codeBlockOptions.navigation && date}
+		<DayNavigation bind:dateOffset bind:date bind:startDate />
+	{/if}
 
+	<div
+		class="gcal-week-container"
+		style:grid-template-columns="auto repeat({codeBlockOptions.timespan},
+		minmax(0, 1fr))"
+	>
+		{#if codeBlockOptions.timespan > 1}
+			<div class="gcal-stop-overflow" />
+			{#each getDatesToDisplay(date) as day, i}
+				<div class="gcal-day-container">
+					<span class="gcal-dayofweek">{day.format('ddd')}</span>
+					<span class="gcal-day">{day.format('D')}</span>
+				</div>
+			{/each}
+		{:else}
+			<!-- Required if no header elements are displayed -->
+			<div class="gcal-stop-overflow" />
+			<div class="gcal-stop-overflow" />
+		{/if}
 
-            {#if codeBlockOptions.showAllDay}
-                <div class="gcal-stop-overflow" />
-                {#each getDatesToDisplay(date) as day, i}
-                    <div class="gcal-stop-overflow">
-                        <AllDayContainer 
-                            {goToEvent}
-                            events={events.filter(e => e.start.date && window.moment(e.start.date).isSame(day, "day"))}
-                        />
-                    </div>
-                {/each}
-            {/if}
-            <div class="gcal-stop-overflow">
-                <TimeLineHourText hourRange={codeBlockOptions.hourRange} />
-            </div>
-            {#each getDatesToDisplay(date) as day, i}
-                <div class="gcal-stop-overflow">
-                    <TimeLine 
-                        events={events.filter(e => e.start.dateTime && window.moment(e.start.dateTime).isSame(day, "day"))}
-                        bind:date 
-                        hourRange={codeBlockOptions.hourRange} 
-                        {goToEvent}
-                    /> 
-                </div>
-            {/each}
-        </div>
-    </div>
-    
+		{#if codeBlockOptions.showAllDay}
+			<div class="gcal-stop-overflow" />
+			{#each getDatesToDisplay(date) as day, i}
+				<div class="gcal-stop-overflow">
+					<AllDayContainer
+						{goToEvent}
+						events={events.filter(
+							(e) =>
+								e.start.date && window.moment(e.start.date).isSame(day, 'day')
+						)}
+					/>
+				</div>
+			{/each}
+		{/if}
+		<div class="gcal-stop-overflow">
+			<TimeLineHourText hourRange={codeBlockOptions.hourRange} />
+		</div>
+		{#each getDatesToDisplay(date) as day, i}
+			<div class="gcal-stop-overflow">
+				<TimeLine
+					events={events.filter(
+						(e) =>
+							e.start.dateTime &&
+							window.moment(e.start.dateTime).isSame(day, 'day')
+					)}
+					{day}
+					hourRange={codeBlockOptions.hourRange}
+					{goToEvent}
+				/>
+			</div>
+		{/each}
+	</div>
+</div>
+
 <style>
-    .gcal-stop-overflow {
-        overflow: hidden;
-        min-width: 0;
-        min-height: 0;
-    }
+	.gcal-stop-overflow {
+		overflow: hidden;
+		min-width: 0;
+		min-height: 0;
+	}
 
-    .gcal-week-container {
-        position: relative;
-        display: inline-grid;
-        gap: 1em;
-        grid-auto-flow: row;
-        overflow: hidden;
-    }
+	.gcal-week-container {
+		position: relative;
+		display: inline-grid;
+		gap: 1em;
+		grid-auto-flow: row;
+		overflow: hidden;
+	}
 
-    .gcal-week-container > * {
-        min-width: 0px;
-        min-height: 0px;
-    }
+	.gcal-week-container > * {
+		min-width: 0px;
+		min-height: 0px;
+	}
 
-    .gcal-day-container {
-        width: 100%;
-        display: grid;
-        justify-content: center;
-        align-items: center;
-    }
+	.gcal-day-container {
+		width: 100%;
+		display: grid;
+		justify-content: center;
+		align-items: center;
+	}
 
-    .gcal-day, .gcal-dayofweek {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: medium;
-    }
+	.gcal-day,
+	.gcal-dayofweek {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: medium;
+	}
 
-    .gcal-day {
-        font-weight: 700;
-        font-size: x-large;
-    }
-    
+	.gcal-day {
+		font-weight: 700;
+		font-size: x-large;
+	}
 </style>
